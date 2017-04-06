c95b27ebdfb5d616b05644724f6a9560e97d2092
serabiasin <ahmadalfiazdzikri@gmail.com>
2017-04-06 22:01:47 +0700

    initial change 1

--- struct_interface.h
+++ struct_interface.h
@@ -1,6 +1,17 @@
 #ifndef STRUCT_INTERFACE_H
 #define STRUCT_INTERFACE_H
 
+struct user_priv{
+  /* data */
+};
 
+/*USER ADMIN LINKING*/
+struct ua_l {
+  /* data */
+};
+
+struct admin_priv {
+  /* data */
+};
 
 #endif
